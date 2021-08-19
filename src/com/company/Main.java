8d3c7ca68c0e66f9d26ff6ba287de7dd01fb1006
Rui Nan <nanruihit@gmail.com>
2021-08-18 20:15:45 -0700

    save

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        testPermuation();
+    }
+
+    private static void testPermuation() {
+        PermutationWithRange permutationWithRange = new PermutationWithRange();
+        System.out.println(permutationWithRange.permuation(new char[]{'a','a', 'b', 'c'}, 2, 3));
     }
 }
---
+++ src/com/company/PermutationWithRange.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.*;
+
+public class PermutationWithRange {
+
+    // 有重复
+    public List<String> permuation (char[] chars, int s, int e) {
+
+        Arrays.sort(chars);
+        boolean[] visited = new boolean[chars.length];
+        List<String> res = new ArrayList<>();
+        helper (res, visited, new StringBuilder(), s, e, chars);
+        return res;
+    }
+
+    void helper (List<String> res, boolean[] visited, StringBuilder sb, int s, int e, char[] chars) {
+        int length = sb.length();
+        if (sb.length() >= s && sb.length() <= e) {
+            res.add(sb.toString());
+            if (sb.length() == e) {
+                return;
+            }
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (i > 0 && chars[i] == chars[i - 1] && !visited[i - 1] || visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            sb.append(chars[i]);
+            helper (res,visited, sb, s, e, chars);
+            visited[i] = false;
+            sb.setLength(length);
+        }
+    }
+}
